8018f730f54d120d786557e7da37e563e3898c53
Sluch <ahmedking10710@gmail.com>
2025-04-16 18:07:24 +0200

    change in index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const connectDB = require('./config/db')
 const cors = require('cors')
 require('dotenv').config()
-
+const {errorhandler} = require('./middelwares/errorHandler')
 // DB Connection
 
 connectDB()
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 app.use("/api/flower" , require('./routes/FlowerRoute'))
 app.use("/api/auth" , require('./routes/UserRoute'))
 app.use("/api/notify" , require('./routes/NotifyRoute'))
-
+app.use(errorhandler)
 // Listen
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}!`)
